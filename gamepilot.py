a5f9ef7a2e5b0406fab09278e2e0edd83b8178be
decorocco <andrebr@al.insper.edu.br>
2019-04-18 16:04:23 -0300

    Teste do Prompt

---
+++ gamepilot.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 18 11:50:54 2019
+
+@author: Aspire_andre
+"""
+
+#game pilot EP1
+print("Você se encontra no meio do saguão de sua universidade, o Insper. Você se lembra que está atrasado no seu EP de Design de software, mas você não está a fim de termina-lo. Você vê seus amigos que já terminaram o projeto saindo para ir ao cinema assistir Guerra Finita:Começato. Você também pode ignora-los e ir à biblioteca.") 
