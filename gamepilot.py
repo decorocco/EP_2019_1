e04cd0908f51dad5460b514b47aa0f9c95e969d8
decorocco <andrebr@al.insper.edu.br>
2019-04-19 16:44:41 -0300

    começo e perg1

--- gamepilot.py
+++ gamepilot.py
@@ -6,6 +6,28 @@ Created on Thu Apr 18 11:50:54 2019
 """
 
 #game pilot EP1
-print("Você se encontra no meio do saguão de sua universidade, o Insper. Você se lembra que está atrasado no seu EP de Design de software, mas você não está a fim de termina-lo. Você vê seus amigos que já terminaram o projeto saindo para ir ao cinema assistir Guerra Finita:Começato. Você também pode ignora-los e ir à biblioteca.\n\nIr à biblioteca = biblioteca \nIr com seus amigos = amigos") 
-data = {"metadata":"Andre","betadata":"Andrew"}
-print(data[metadata])
+import time
+import random as rd
+inventario = []
+print("Você se encontra no meio do saguão de sua universidade, o Insper.")
+nome_jogador = input("Qual o seu nome, honorável sofredor universitário? >>")
+time.sleep(1)
+print()
+print("Você se lembra que está atrasado na entrega de seu EP de Design de software, mas você não está a fim de termina-lo.")
+print("Você vê seus amigos que já terminaram o projeto saindo para ir ao cinema assistir Guerra Finita:Começato.")
+print("Você também pode ignora-los e ir à biblioteca.\n\nIr à biblioteca = biblioteca \nIr com seus amigos ao cinema = cinema") 
+
+def escolha1():
+    resposta1 = input("O que deseja fazer, {}? >>".format(nome_jogador)).strip
+    while resposta1 != "biblioteca" and resposta1 != "cinema":
+        print("Resposta inválida!")
+        resposta1 = input("O que deseja fazer, {}? >>".format(nome_jogador)).strip
+    if resposta1 == "biblioteca":
+        #funcao biblioteca
+        print("Você se dirige à biblioteca")
+    elif resposta1 == "cinema":
+        #funcao cinema
+        print("você alcança seus amigos e parte para o cinema")
+    return resposta1
+    
+escolha1()
