2669a08c4ca5a46eefb29deedf7bfcad2c378966
decorocco <andrebr@al.insper.edu.br>
2019-04-19 15:00:51 -0300

    teste 2 pilot

--- gamepilot.py
+++ gamepilot.py
@@ -6,4 +6,6 @@ Created on Thu Apr 18 11:50:54 2019
 """
 
 #game pilot EP1
-print("Você se encontra no meio do saguão de sua universidade, o Insper. Você se lembra que está atrasado no seu EP de Design de software, mas você não está a fim de termina-lo. Você vê seus amigos que já terminaram o projeto saindo para ir ao cinema assistir Guerra Finita:Começato. Você também pode ignora-los e ir à biblioteca.") 
+print("Você se encontra no meio do saguão de sua universidade, o Insper. Você se lembra que está atrasado no seu EP de Design de software, mas você não está a fim de termina-lo. Você vê seus amigos que já terminaram o projeto saindo para ir ao cinema assistir Guerra Finita:Começato. Você também pode ignora-los e ir à biblioteca.\n\nIr à biblioteca = biblioteca \nIr com seus amigos = amigos") 
+data = {"metadata":"Andre","betadata":"Andrew"}
+print(data[metadata])
